e476b0710eb7776aed12da516429d27c0d2b5a9e
Kelly Netterville <36lima@gmail.com>
2024-01-17 14:04:41 -0600

    Initial project config

---
+++ src/main/java/com/scnetter/taskmanagementapp/TaskManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.scnetter.taskmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class lsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(lsAppApplication.class, args);
+	}
+
+}
